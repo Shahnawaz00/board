9f01200a821b2ce1a860f852bebedf2a4623e69b
Shahnawaz00 <shahnawazc987@gmail.com>
2022-07-14 21:32:21 +1000

    fixed directory path in api

--- pages/api/users/profile/getComments.js
+++ pages/api/users/profile/getComments.js
@@ -1,4 +1,4 @@
-import prisma from "../../../lib/prisma";
+import prisma from "../../../../lib/prisma";
 
 
 //get all comments made by the user and order by date
--- pages/api/users/profile/getDiscussions.js
+++ pages/api/users/profile/getDiscussions.js
@@ -1,4 +1,4 @@
-import prisma from "../../../lib/prisma";
+import prisma from "../../../../lib/prisma";
 
 
 //get all discussions made by the user
